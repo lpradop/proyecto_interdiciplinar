e51715cce9e0e9eb10c4cab44d81131a91e01e8c
Brocolio de la chunsa <lpradop@unsa.edu.pe>
2020-07-17 12:36:31 -0500

    se corregieron nombres, cambio menor en la estructura

--- client_app/client.py
+++ client_app/client.py
@@ -12,20 +12,22 @@ class Client():
         self.run()
 
     def run(self):
+        interface_state = ["login", "admin", "teacher"]
 
         def createLoginInterface(self):
+            font_family = "Vrinda"
+            font_style = tkFont.Font(
+                family=font_family, size=30, weight='bold')
+            font_style2 = tkFont.Font(family=font_family, size=13)
 
-            login_frame = ttk.LabelFrame(self.main_window)
+            login_frame = ttk.Frame(self.main_window)
             login_frame.pack(fill=tk.BOTH, padx=120, pady=100)
 
-            fontStyle = tkFont.Font(family="Vrinda", size=30, weight='bold')
-            titulo = ttk.Label(login_frame, text="S.C.A.D", font=fontStyle)
+            titulo = ttk.Label(login_frame, text="S.C.A.D", font=font_style)
             titulo.grid(column=1, row=0, padx=5, pady=30)
 
-            fontStyle2 = tkFont.Font(family="Vrinda", size=13)
-
             login_username = ttk.Label(
-                login_frame, text="Usuario", font=fontStyle2)
+                login_frame, text="Usuario", font=font_style2)
             login_username.grid(column=0, row=1, padx=5, pady=10)
 
             username_entry = ttk.Entry(login_frame)
@@ -33,21 +35,22 @@ class Client():
                                 pady=10, ipadx=20, ipady=2)
 
             login_password = ttk.Label(
-                login_frame, text="Contraseña", font=fontStyle2)
+                login_frame, text="Contraseña", font=font_style2)
             login_password.grid(column=0, row=2, padx=5, pady=10)
 
             password_entry = ttk.Entry(login_frame, show="*")
             password_entry.grid(column=1, row=2, padx=5,
                                 pady=10, ipadx=20, ipady=2)
 
-            login_button = ttk.Button(login_frame, text="   Iniciar Sesion   ")
+            login_button = ttk.Button(
+                login_frame, text="   Iniciar Sesion   ", command=makeRequest)
             login_button.grid(column=1, row=3, padx=5, pady=15)
 
             # crear todos los elementos que tendra el la interfaz de login
 
             while True:
-                self.main_window.update()  # una vez creados se dibujaran en pantalla
                 self.main_window.update_idletasks()
+                self.main_window.update()  # una vez creados se dibujaran en pantalla
 
         def createTeachInterface(self):
             # crear todos los elementos que tendra la interfaz donde se marca la asistencia
@@ -56,6 +59,9 @@ class Client():
         def createAdminInterface(self):
             # interfaz que vera el admin
             pass
+
+        def makeRequest():
+            print("realizando request")
 
         createLoginInterface(self)
         # una vez creada la interface de login, colocar la ventana en modo de escucha(listen)
