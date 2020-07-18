f549a3cab549e40ec3a8c54b61a50e2948748d7a
Brocolio de la chunsa <lpradop@unsa.edu.pe>
2020-07-17 22:59:46 -0500

    esqueleto definido

--- server_app/main.py
+++ server_app/main.py
@@ -1,10 +1,35 @@
 from flask import Flask
 from flask import jsonify
-from flask import make_response
+from flask import request
 
 app = Flask(__name__)
 
 
-@app.route("/login",methods=['GET'])
-def login():
-    return jsonify(token="4u6e4o6a4u6")
+@app.route("/login", methods=['GET'])
+def login() -> dict:
+    data: dict = request.json
+    # consulta a la base de datos si el usuario y contrasena son validos
+    if(True):
+        # iniciar session
+        return {"success": True}
+    else:
+        return {"success": False}
+
+
+@app.route("/teacher_course_list", methods=['GET'])
+def teacherCourseList():
+    course_list: dict
+    # consultar la lista de cursos usando del usuario
+
+    return course_list
+
+
+@app.route("/teacher_mark", methods=['POST'])
+def teacherMark():
+
+    data: dict = request.json
+    #validar si es posible marcar el registro del curso
+    if(True):
+        return {"success": True}
+    else:
+        return {"success": False}
