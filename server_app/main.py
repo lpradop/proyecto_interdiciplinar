d4a4c081231bfb8f7c1dace141c5621b445a9206
Brocolio de la chunsa <lpradop@unsa.edu.pe>
2020-07-18 20:13:25 -0500

    login implementado

--- server_app/main.py
+++ server_app/main.py
@@ -1,34 +1,50 @@
 from flask import Flask
 from flask import jsonify
 from flask import request
+from flask import session
+from datetime import timedelta
 import mysql.connector as sql
 
 app = Flask(__name__)
+app.secret_key = "clave ultra secreta"
+app.permanent_session_lifetime = timedelta(minutes=10)
 db = sql.connect(
     host="localhost",
     user="brocolio",
-    password="brocolio"
+    password="brocolio",
+    database="scad"
 )
-db_cursor = db.cursor()
+db_cursor = db.cursor(dictionary=True)
 
 
 @app.route("/login", methods=['GET'])
 def login() -> dict:
-    data: dict = request.json
+    data = request.get_json()
     # consulta a la base de datos si el usuario y contrasena son validos
-    if(True):
-        # iniciar session
-        return {"success": True}
-    else:
+    query: str = "select * from Docente where Usuario={} and Contrasena={};".format(
+        data["Usuario"], data["Contrasena"])
+    response = db_cursor.execute(query)
+
+    if(len(response) == 0):
+        # no valido
         return {"success": False}
+    else:
+        # valido, proceder a crear la sesion
+        session = response[0]
+        session.permanent = True
+
+        return {"success": True}
 
 
 @app.route("/teacher_course_list", methods=['GET'])
 def teacherCourseList() -> dict:
-    course_list: dict
-    # consultar la lista de cursos usando del usuario
+    course_list: list
+    # consultar la lista de cursos usando la session
+    if len(session == 0):
+        return {}
+    else:
 
-    return course_list
+        return course_list
 
 
 @app.route("/teacher_mark", methods=['POST'])
